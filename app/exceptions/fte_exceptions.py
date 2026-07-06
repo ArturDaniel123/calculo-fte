d4e5f3468e7ae1bbb8b91492d437558ac48be751
ArturDaniel123 <artur.daniel.borges2@hotmail.com>
2026-07-06 11:06:34 -0300

    chore: criado repositório 'exceptions'

---
+++ app/exceptions/fte_exceptions.py
@@ -0,0 +1,3 @@
+class FTEValidationError(Exception):
+    def __init__(self, message: str):
+        self.message = message
--- app/services/logica_fte.py
+++ app/services/logica_fte.py
@@ -1,27 +1,29 @@
+from app.exceptions.fte_exceptions import FTEValidationError
+
 def calcular_fte(salario_hora, tempo_tarefa, repeticao, tempo_bot,
                  custo_mensal_bot=0):
     '''Realiza toda a lógica do cálculo do FTE'''
     if salario_hora <= 0:
-        raise ValueError("O salário não pode ser igual ou menor que zero.")
+        raise FTEValidationError("O salário não pode ser igual ou menor que zero.")
     salario_minuto = salario_hora/60
     
     if tempo_tarefa <= 0:
-        raise ValueError("O tempo de realização da tarefa não pode ser igual ou menor que zero.")   
+        raise FTEValidationError("O tempo de realização da tarefa não pode ser igual ou menor que zero.")   
     custo_por_tarefa_humana = tempo_tarefa * salario_minuto
     
     if repeticao <= 0:
-        raise ValueError("O número de repetições não pode ser nulo.") 
+        raise FTEValidationError("O número de repetições não pode ser nulo.") 
     custo_diario_humano = custo_por_tarefa_humana * repeticao
     
     custo_semanal_humano = custo_diario_humano * 5 # Segunda a sexta
     custo_mensal_humano = custo_semanal_humano * 4 # 4 semanas
 
     if custo_mensal_bot < 0:
-        raise ValueError("Custo mensal bot inválido.")
+        raise FTEValidationError("Custo mensal bot inválido.")
     econ_liq_mes = custo_mensal_humano - custo_mensal_bot # Economia liquída do mês
 
     if tempo_bot <= 0:
-        raise ValueError("O tempo de execução de tarefa não pode ser nulo.")
+        raise FTEValidationError("O tempo de execução de tarefa não pode ser nulo.")
     produtividade_relativa = tempo_tarefa / tempo_bot
 
     return {
