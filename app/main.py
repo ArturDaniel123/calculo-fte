c5e8f0450799d4271ca93797ec09344caef446ff
ArturDaniel123 <artur.daniel.borges2@hotmail.com>
2026-07-06 11:22:10 -0300

    feature: adicionados tratamentos de erro de validação

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from app.exceptions.fte_exceptions import FTEValidationError
 from app.routers.fte import router as fte_router
 
 app = FastAPI()
+
+@app.exception_handler(FTEValidationError)
+def handle_fte_error(request: Request, exc: FTEValidationError):
+    return JSONResponse(
+        status_code=400,
+        content={
+            "error": True,
+            "type": "fte_validation_error",
+            "message": exc.message
+        }
+    )
+
+
 
 app.include_router(fte_router, prefix="/fte") # Coloca as rotas dentro da aplicação
 
